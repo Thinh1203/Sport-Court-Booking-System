e4e058807b1476ba4a5b4d5d2796652c99d4b790
Thinh Quach <thinhquach1203@gmail.com>
2024-09-04 10:04:39 +0700

    update coupon api

--- src/coupon/swagger/index.ts
+++ src/coupon/swagger/index.ts
@@ -1,7 +1,7 @@
 import { ApiBody } from '@nestjs/swagger';
 
 export const createCouponApiBody = ApiBody({
-  description: 'Amenity data with image file',
+  description: 'Coupon data with image file',
   schema: {
     type: 'object',
     properties: {
@@ -25,7 +25,8 @@ export const createCouponApiBody = ApiBody({
       discountValue: {
         type: 'number',
         example: 10,
-        description: 'percentage_value: 10-20-30 or flat_value: 100000 - 200000 - 300000',
+        description:
+          'percentage_value: 10-20-30 or flat_value: 100000 - 200000 - 300000',
       },
       startDate: {
         type: 'string',
@@ -79,6 +80,97 @@ export const createCouponApiBody = ApiBody({
         description: 'Image file for the coupon',
       },
     },
-    required: ['code', 'description', 'isDynamic', 'discountType', 'discountValue', 'startDate', 'endDate', 'maxUsage', 'minTotalAmount', 'status', 'theSportsCenterId']
+    required: [
+      'code',
+      'description',
+      'isDynamic',
+      'discountType',
+      'discountValue',
+      'startDate',
+      'endDate',
+      'maxUsage',
+      'minTotalAmount',
+      'status',
+      'theSportsCenterId',
+    ],
+  },
+});
+
+export const updateCouponApiBody = ApiBody({
+  description: 'Update Coupon',
+  schema: {
+    type: 'object',
+    properties: {
+      code: {
+        type: 'string',
+        example: 'COMPANYTRIP2024',
+      },
+      description: {
+        type: 'string',
+        example: 'Incredible hot deal when booking company trip',
+      },
+      isDynamic: {
+        type: 'boolean',
+        example: true,
+      },
+      discountType: {
+        type: 'string',
+        example: 'flat, percentage',
+        description: 'percentage: 10% - 20% - 30%. flat: 100k - 200k- 300k',
+      },
+      discountValue: {
+        type: 'number',
+        example: 10,
+        description:
+          'percentage_value: 10-20-30 or flat_value: 100000 - 200000 - 300000',
+      },
+      startDate: {
+        type: 'string',
+        example: '2024-08-16T12:00:00',
+      },
+      endDate: {
+        type: 'string',
+        example: '2024-10-18T12:00:00',
+      },
+      maxUsage: {
+        type: 'number',
+        example: 1000,
+      },
+      minTotalAmount: {
+        type: 'number',
+        example: 1,
+        description: 'Minimum order amount to apply discount code',
+      },  
+      status: {
+        type: 'string',
+        example: 'published',
+      },
+      MaximumDiscountTotal: {
+        type: 'number',
+        example: 100000,
+        description:
+          'The maximum discount amount that the coupon code can apply. Only meaningful for percentage discounts.',
+      },
+      metaData: {
+        type: 'object',
+        example: {
+          applicableCourts: ['VIP', 'Normal'],
+          courtRestrictions: {
+            VIP: {
+              extraDiscount: 5,
+              maxUsagePerUser: 1,
+            },
+            Normal: {
+              maxUsagePerUser: 3,
+            },
+          },
+        },
+      },
+      file: {
+        type: 'string',
+        format: 'binary',
+        description: 'Image file for the coupon',
+      },
+    },
   },
 });
