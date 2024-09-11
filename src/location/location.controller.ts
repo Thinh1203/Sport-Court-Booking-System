d5f0a7c59b13c5af8656aaeba09a1bd1523c3053
Thinh Quach <thinhquach1203@gmail.com>
2024-09-11 17:04:16 +0700

    create location service

--- src/location/location.controller.ts
+++ src/location/location.controller.ts
@@ -1,10 +1,59 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpStatus,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { LocationDto } from './dto/location.dto';
+import { Response } from 'express';
+import { LocationService } from './location.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('location')
 export class LocationController {
-    constructor() {}
+  constructor(private readonly locationService: LocationService) {}
 
-    async addNewLocation() {
-        
+  @UseGuards(AuthGuard)
+  async addNewLocation(
+    @Res() res: Response,
+    @Req() req: Request,
+    @Body() locationDto: LocationDto,
+  ) {
+    try {
+      const user = req['user'];
+      const data = await this.locationService.addNewLocation(
+        locationDto,
+        Number(user.id),
+      );
+      return res.status(HttpStatus.OK).json({
+        statusCode: HttpStatus.OK,
+        data,
+      });
+    } catch (error) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: error.message,
+      });
     }
+  }
+  @UseGuards(AuthGuard)
+  async getLocationByUserId(@Res() res: Response, @Req() req: Request) {
+    try {
+      const user = req['user'];
+      const data = await this.locationService.getLocationByUserId(
+        Number(user.id),
+      );
+      return res.status(HttpStatus.OK).json({
+        statusCode: HttpStatus.OK,
+        data,
+      });
+    } catch (error) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: error.message,
+      });
+    }
+  }
 }
--- src/location/location.service.ts
+++ src/location/location.service.ts
@@ -1,13 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { LocationDto } from './dto/location.dto';
 
 @Injectable()
 export class LocationService {
-    constructor(
-        private readonly prisma: PrismaService
-    ) {}
-    // async addNewLocation(locationDto: LocationDto, userId: number) {
-    //     const existingLocation = await this.prisma.userLocation()
-    // }
+  constructor(private readonly prisma: PrismaService) {}
+  async addNewLocation(locationDto: LocationDto, userId: number) {
+    const existingUser = await this.prisma.user.findFirst({
+      where: {
+        id: userId,
+      },
+    });
+    if (!existingUser) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+    const existingLocation = await this.prisma.userLocation.findFirst({
+      where: {
+        placeId: locationDto.place_id,
+      },
+    });
+    if (existingLocation) {
+      throw new HttpException('Location already exists', HttpStatus.CONFLICT);
+    }
+    return await this.prisma.userLocation.create({
+      data: {
+        latitude: locationDto.lat,
+        longtitude: locationDto.lon,
+        name: locationDto.name,
+        displayName: locationDto.display_name,
+        placeId: locationDto.place_id,
+        userId: userId,
+      },
+    });
+  }
+
+  async getLocationByUserId(userId: number) {
+    const existingUser = await this.prisma.user.findMany({
+      where: {
+        id: userId,
+      },
+    });
+    if (!existingUser) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+    return await this.prisma.userLocation.findMany({
+      where: {
+        userId,
+      },
+    });
+  }
 }
